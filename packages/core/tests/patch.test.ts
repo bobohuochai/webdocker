225c92a7722bdce585b1155700da99f414f86302
徐军 <xujun@pingpongx.com>
2022-09-16 16:42:40 +0800

    feat: + setInterval patcher unit test

---
+++ packages/core/tests/patch.test.ts
@@ -0,0 +1,27 @@
+import setIntervalPatch from '../src/patch/interval';
+
+function sleep(ms:number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+test('setInterval patcher work', async () => {
+  const free = setIntervalPatch(window);
+  const clearedListener = jest.fn();
+  const unclearedListener = jest.fn();
+  const unclearedListenerWithArgs = jest.fn();
+
+  const interval1 = window.setInterval(clearedListener, 60);
+  window.setInterval(unclearedListener, 8);
+  window.setInterval(unclearedListenerWithArgs, 30, 'hello');
+  window.clearInterval(interval1);
+
+  await sleep(20);
+
+  free();
+
+  expect(clearedListener).toBeCalledTimes(0);
+  expect(unclearedListener).toBeCalledTimes(2);
+  expect(unclearedListenerWithArgs).toBeCalledTimes(0);
+});
