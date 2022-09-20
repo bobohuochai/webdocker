37e1057c66fe87a811a381993cb13ef345cf4eed
徐军 <xujun@pingpongx.com>
2022-09-20 17:37:24 +0800

    feat: 【vue-host-application】+ loading,error 异常情况处理

--- packages/vue/vue-application/src/application.js
+++ packages/vue/vue-application/src/application.js
@@ -32,10 +32,12 @@ export default {
       loading: true,
       hasError: false,
       app: null,
+      error: null,
+      unmounted: false,
     };
   },
   render(h) {
-    if (this.error) {
+    if (this.hasError) {
       if (this.$slots.error) {
         return this.$slots.error;
       }
@@ -52,21 +54,48 @@ export default {
     });
   },
   async mounted() {
-    const { config, lifecycles } = getWrapProps(this.$props);
+    this.executeAction('mount', async () => {
+      const { config, lifecycles } = getWrapProps(this.$props);
+      this.app = await webdocker.loadApp({
+        container: this.$refs.el,
+        name: this.name,
+        entry: this.manifest,
+      }, config, lifecycles);
 
-    this.app = await webdocker.loadApp({
-      container: this.$refs.el,
-      name: this.name,
-      entry: this.manifest,
-    }, config, lifecycles);
-
-    this.app.mount();
-    this.loading = false;
-    this.$emit('microAppDidMount');
+      this.app.mount();
+      this.loading = false;
+      this.$emit('microAppDidMount');
+    });
   },
   beforeDestroy() {
-    if (this.app) {
-      this.app.unmount();
-    }
+    this.executeAction('unmount', () => {
+      if (this.app) {
+        this.app.unmount();
+      }
+    });
+    this.unmounted = true;
   },
+  methods: {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    executeAction(action, thing = () => {}) {
+      if (this.hasError && action !== 'unmount') {
+        return;
+      }
+      Promise.resolve().then(() => {
+        if (this.unmounted && action !== 'unmount') {
+          return;
+        }
+
+        thing();
+      }).catch((err) => {
+        const error = new Error(`During '${action}',sub micro application threw an error:${err.message}`);
+        this.hasError = true;
+        this.loading = false;
+        this.error = error;
+        this.$emit('microAppDidCatch', error);
+        console.error(error);
+      });
+    },
+  },
+
 };
